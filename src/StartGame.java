5b6d87b0308d67e2add3bb974c3980c01a0cf37a
Shivali\Shivali <shivalibhagat1@gmail.com>
2016-09-15 21:53:30 +0530

    no message

--- src/StartGame.java
+++ src/StartGame.java
@@ -1,23 +1,17 @@
-/*
- * StartGame.java is the gui for our project
- * User enters the coordinates and the main program calls input()
- * That's all this class does
- * 
- */
+
 public class StartGame {
 	public static void main (String args[])
 	{
 	GameOfLife g=new GameOfLife();
-	System.out.println("Enter : ");
+	System.out.println("Enter (x,y) co-ordinates : ");
 	g.input();
-	
-	/*g.displayGrid();
-	g.nextStep();
+	g.fillGrid();
+	g.displayGrid();
+	g.nextGeneration();
 	System.out.println("Display :");
 	g.displayGrid();
-	*/	
+		
 		
 	}
 
-	
 }
